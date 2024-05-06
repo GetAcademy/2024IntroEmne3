cf6914720ea2ea97b11d9a9699991d163e3c0a4c
GET Academy <terje@kolderup.net>
2024-05-06 11:22:20 +0200

    Initial commit

---
+++ 2024IntroEmne3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+- eksempel med input 
+- GitHub
+- Skjelett med klasse og metode
+*/
+
+Console.Write("Hva heter du? ");
+string name = Console.ReadLine();
+Console.Write("I hvilket år er du født? ");
+string birthYearStr = Console.ReadLine();
+int birthYear = Convert.ToInt32(birthYearStr);
+int age = 2024 - birthYear;
+Console.Write("Har du hatt bursdag i år (j/n)? ");
+string answer = Console.ReadLine();
+char letter = answer[0];
+if (letter == 'n')
+{
+    age--;
+}
+
+Console.WriteLine($"Hei, {name}!");
+Console.WriteLine($"Du er {age} år gammel.");
+
